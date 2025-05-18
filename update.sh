b5ec8f4653147b382f4550a26c215a7bd847cb5c
Kartik Ohri <kartikohri13@gmail.com>
2025-05-18 15:56:27 +0530

    Add GitHub Actions workflow for PyPI publishing
    The workflow carries out the following steps:
    
    1. Check out the repository.
    2. Install Python.
    3. Install the build package.
    4. Build the package.
    5. Publish the package to pypi.
    
    The publishing process does not require the use of any credentials or
    keys as it utilizes Pypi's "Trusted Publishing" support. See also:
    https://docs.pypi.org/trusted-publishers/.
    
    As publication is now handled by the GitHub action, remove it from the
    update script.

--- update.sh
+++ update.sh
@@ -48,13 +48,6 @@ then
   exit 69
 fi
 
-if ! type twine &>/dev/null
-then
-  echo Error: twine command not found
-  echo Please install twine package
-  exit 69
-fi
-
 ################################################################################
 echo Setting virtual environment up
 
@@ -63,7 +56,8 @@ unset python_command
 set +u
 source venv/bin/activate
 set -u
-pip install -r requirements.txt -U
+pip install -U pip
+pip install --group dev
 python_version=`python --version`
 
 ################################################################################
@@ -90,39 +84,14 @@ git commit mbrng/ musicbrainz_mmd.xsd \
 ################################################################################
 echo Testing
 
-pip install -r requirements-test.txt -U
-python -m pytest
+pip install --group test
+pytest
 
 ################################################################################
-echo Creating git tag
-
-minor=`echo "$mmd_schema_version" \
-  | sed 's/^v-\([0-9]\{1,\}\)-\([0-9]\{1,\}\)-\([0-9]\{1,\}\)/\1\2\3/'`
-version="2.${minor}."`git tag --list "v-2.${minor}.*" | wc -l`
-tag="v-$version"
-git tag -u CE33CF04 "$tag" \
-  -m "Regenerated upon MMD schema 2.0 $mmd_schema_version with $python_version."
-
-################################################################################
-echo Pushing git commits and tag
+echo Pushing git commits
 
 git push origin master
-git push origin "$tag"
 
-################################################################################
-echo Building and pushing Python package distribution
-
-set -x
-
-python -m pip install build
-python -m build
-gpg -u CE33CF04 --detach-sign -a dist/mb-rngpy-${version}.tar.gz
-twine upload \
-  --config-file ~/.pypirc \
-  -c "Python bindings for MMD schema 2.0 $mmd_schema_version" \
-  dist/mb-rngpy-${version}.tar.gz{,.asc}
-
-################################################################################
 echo Done
 
 deactivate
